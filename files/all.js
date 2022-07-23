b3594aaed21affe47de5b2a8980c0a452e23a917
neohyntw <neohyntw@gmail.com>
2022-07-23 10:11:46 +0800

    up

--- files/all.js
+++ files/all.js
@@ -42,5 +42,13 @@ $(document).ready(function(){
 	  },
 	});
 
+	$('.top a').click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		  $('html,body').animate({
+		    scrollTop: 0
+		  }, 1000);
+	});
+
 });
 
